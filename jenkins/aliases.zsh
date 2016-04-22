1db7b0e5caaa0e6b6f39fe1858e7d148faa33b3c
shanshanyang <office.ss.nyc@gmail.com>
2016-04-22 11:43:42 -0400

    add jenkins

---
+++ jenkins/aliases.zsh
@@ -0,0 +1,5 @@
+## Jenkins Start
+# sudo launchctl load /Library/LaunchDaemons/org.jenkins-ci.plist
+
+## Jenkins shutdown
+# sudo launchctl unload /Library/LaunchDaemons/org.jenkins-ci.plist
--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,1 @@
-export EDITOR='visual-studio-code'
+export EDITOR='atom'
